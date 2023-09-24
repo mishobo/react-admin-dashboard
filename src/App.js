d545cb7a7a3434ea37790a29fc26e286389b8cf5
mishobo <husseinabdalla287@gmail.com>
2023-09-24 14:34:52 +0300

    team data in a table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Sidebar from "./scenes/global/Sidebar";
 import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+
 
 export default function App() {
   const [theme, colorMode] = useMode();
@@ -16,6 +20,10 @@ export default function App() {
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
