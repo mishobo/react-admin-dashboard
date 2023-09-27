a518d06a2f84f883fea27479e6027ba871d7b31d
mishobo <husseinabdalla287@gmail.com>
2023-09-27 12:40:19 +0300

    FAQ

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form"
 import Calendar from "./scenes/calendar/calendar";
+import FAQ from "./scenes/faq";
 
 
 export default function App() {
@@ -31,6 +32,7 @@ export default function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
             </Routes>
           </main>
         </div>
